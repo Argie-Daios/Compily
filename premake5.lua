9ebacc3906fbd50143e13482d733b5b6c5588547
Argie <142118009+Argie-Daios@users.noreply.github.com>
2025-07-09 02:12:57 +0300

    Initial Commit

---
+++ premake5.lua
@@ -0,0 +1,56 @@
+workspace "Lexy"
+	platforms { "x86", "x64" }
+	configurations { "Debug", "Release" }
+
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.architecture}";
+
+	IncludeDir = {}
+
+project "Lexy"
+	location "Lexy"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+	}
+
+	filter "system:windows"
+    	systemversion "latest"
+    	defines { "LEXY_WINDOWS" }
+
+	filter "system:linux"
+   		defines { "LEXY_LINUX" }
+
+	filter "platforms:x86"
+		architecture "x86"
+
+	filter "platforms:x64"
+		architecture "x64"
+
+	filter "configurations:Debug"
+		defines "LEXY_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LEXY_RELEASE"
+		runtime "Release"
+		optimize "On"
